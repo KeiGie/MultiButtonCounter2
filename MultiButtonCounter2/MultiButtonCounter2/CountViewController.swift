31acf01579f24674be55735d528c6990e699c86f
西牧恭平 <k_n_0825@yahoo.co.jp>
2017-11-23 17:02:04 +0900

    メインのViewControllerの名前を変更。

---
+++ MultiButtonCounter2/MultiButtonCounter2/CountViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MultiButtonCounter2
+//
+//  Created by 西牧恭平 on 2017/11/23.
+//  Copyright © 2017年 西牧恭平. All rights reserved.
+//
+
+import UIKit
+
+class CountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
